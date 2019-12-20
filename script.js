0d5220708be021b82e58c65a56f04b02c09fcf2b
JayDub21 <justin.wofford.21@gmail.com>
2019-12-19 20:01:09 -0600

    saveSlot buttons added

---
+++ script.js
@@ -0,0 +1,10 @@
+var now = moment();
+$('time').each(function (i, e) {
+    var time = moment($(e).attr('datetime'));
+
+    if (now.diff(time, 'days') <= 1) {
+        $(e).html('<span>' + time.from(now) + '</span>');
+    }
+});
+console.log(now);
+
