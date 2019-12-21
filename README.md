2fbfce3ca89eb5dc00d59b6f8642cd7fbe745251
JayDub21 <justin.wofford.21@gmail.com>
2019-12-20 19:54:08 -0600

    picture add and save button local drive attempt

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dayplanner
+Day-Planner
+
+This planner is being designed to help organize all the tasks that somebody would need to be completed in a typical work day time frame.
+
+This document contains html, css, and javascript.
+
+It is further assisted by using bootstrap, query and moment.
+
+
+
+The main framework of the page was designed with html and bootstrap.
+
+The live clock is provided via moment.
+
+The background color is provided via a combination of moment and jquery.
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     //display current time and date in jumbotron.
     var currentDate = $("#currentDate");
     var colorChangeEl = $(".color-change");
+    var saveBtn = $(".saveBtn");
 
     currentDate.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
@@ -25,8 +26,47 @@ $(document).ready(function () {
     }
     //check every second.
     setInterval(update, 1000);
-    $(".textField").text("");
 
+
+
+    //save text to local storage. 
+    saveBtn.on("click", function (event) {
+
+        var button = $(this);
+
+        var btnData = $(this).attr("saveBtn");
+        //console log click on THIS specific button in this class
+        // console.log(btnData);
+        //descriptionTarget locates the specific description element that user filled in text with.
+        var textTarget = $("#textSlot" + btnData);
+        // console.log(textTarget);
+        //we log to ensure we're grabbing the user input.
+        console.log(textTarget.val());
+
+        //this is supposed to set the text area text to that specific textArea and store it.
+        //We pick the "id" attribute from the element, descriptionTarget. This id is the key, and the key value is descriptionTarget.val().
+        localStorage.setItem(textTarget.attr("id"), textTarget.val());
+    });
+
+    //site should load any saved data from localStorage.
+    $("#textSlot9").text(localStorage.getItem("textSlot1"));
+    $("#textSlot10").text(localStorage.getItem("textSlot2"));
+    $("#textSlot11").text(localStorage.getItem("textSlot3"));
+    $("#textSlot12").text(localStorage.getItem("textSlot4"));
+    $("#textSlot13").text(localStorage.getItem("textSlot5"));
+    $("#textSlot14").text(localStorage.getItem("textSlot6"));
+    $("#textSlot15").text(localStorage.getItem("textSlot7"));
+    $("#textSlot16").text(localStorage.getItem("textSlot8"));
+    $("#textSlot17").text(localStorage.getItem("textSlot9"));
+
+    console.log(localStorage)
+
+
+
+    $("#clearBtn").on("click", function () {
+        localStorage.clear();
+        $("#textSlot").text("");
+    });
 });
 
 
